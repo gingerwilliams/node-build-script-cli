4d1f61e77de4d35562329db506370e80cfae2a84
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2020-02-24 17:31:30 -0500

    Create a New Directory in NodeJS

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,50 @@
 #!/usr/bin/env node
 const message = console.log('Hello My Love again')
 
-
 // const program = require('commander');
 // const argv = console.log(program.parse(process.argv))
 // const commands = console.log(process)
 
 const fs = require('fs');
 
-const callback = (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-}
-// fs.writeFile('filename', 'fileContents', 'utf8', callback);
-const write = fs.writeFile('message.js', '//Hello Node.js', 'utf8', callback);
-
-module.exports = {
-    message: message,
-    write: write
-    //commands: commands
+// { recursive: true } was the key
+const createDir = (dirPath) => {
+    fs.mkdir(dirPath, { recursive: true }, (err) => {
+        if (err) throw err;
+        console.log('The dir has been created!');
+    })
 }
 
-//start 
+const createFile = (filePath, fileName, fileConent) => {
+    fs.writeFile(`${filePath}/${fileName}` , fileConent, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+path = './src';
+file = 'index.js'
+content = '//Hello Node.js';
+
+createDir(path);
+createFile(path,file, content)
+
+
+
+
+
+
+// console.log('test fs.mkdir', directory)
+
+
+
+// module.exports = {
+//     //directory: createDirectory,
+//     message: message,
+//     //write: write
+//     //commands: commands
+// }
+
+
 
 
