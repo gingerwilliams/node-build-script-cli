beaf863d6c161ed48a4656edbf506a108ec03c03
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2020-02-24 19:37:25 -0500

    generae a list of directories and files

--- bin/index.js
+++ bin/index.js
@@ -8,43 +8,43 @@ const message = console.log('Hello My Love again')
 const fs = require('fs');
 
 // { recursive: true } was the key
-const createDir = (dirPath) => {
-    fs.mkdir(dirPath, { recursive: true }, (err) => {
-        if (err) throw err;
-        console.log('The dir has been created!');
+const createDir = (dirs) => {
+    dirs.map(dirPath => {
+        fs.mkdir(dirPath, { recursive: true }, (err) => {
+            if (err) throw err;
+            console.log('The dir has been created!');
+        })
     })
 }
 
-const createFile = (filePath, fileName, fileConent) => {
-    fs.writeFile(`${filePath}/${fileName}` , fileConent, 'utf8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
+const createFile = (files) => {
+    files.map(file => {
+        
+        fs.writeFile(`${file}`, `//Hello ${file}`, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    })
 }
 
-path = './src';
-file = 'index.js'
-content = '//Hello Node.js';
+rootPath = './app-name'; //dynamic
 
-createDir(path);
-createFile(path,file, content)
+//list of directories
+const dirs = [
+    `${rootPath}/dist`,
+    `${rootPath}/src`,
+]
 
+const files = [
+    `${rootPath}/webpack.config.js`,
+    `${rootPath}/package.json`,
+    `${rootPath}/src/index.js`,
+    `${rootPath}/src/index.html`,
+    `${rootPath}/dist/index.html`,
+]
 
-
-
-
-
-// console.log('test fs.mkdir', directory)
-
-
-
-// module.exports = {
-//     //directory: createDirectory,
-//     message: message,
-//     //write: write
-//     //commands: commands
-// }
-
+createDir(dirs);
+createFile(files);
 
 
 
