2699883cf7ed8d2ebad421428ae4cbec62d7dcd3
Ginger Williams <gingerwilliams@MacBook-Pro-2.attlocal.net>
2020-03-04 11:16:38 -0500

    generate children and grandchildren files and directories

--- bin/index.js
+++ bin/index.js
@@ -1,50 +1,74 @@
 #!/usr/bin/env node
-const message = console.log('Hello My Love again')
-
-// const program = require('commander');
-// const argv = console.log(program.parse(process.argv))
-// const commands = console.log(process)
+const message = console.log('Hello My Love again');
 
 const fs = require('fs');
 
-// { recursive: true } was the key
-const createDir = (dirs) => {
-    dirs.map(dirPath => {
-        fs.mkdir(dirPath, { recursive: true }, (err) => {
-            if (err) throw err;
-            console.log('The dir has been created!');
-        })
+//returns an array of the directory // computation
+const readDir = (path) => {
+    return fs.readdirSync(path, {}, (err, files) => {
+        if (err) throw err;
+        else if (files.legnth === 0) return 'the directory is empty'
+        console.log('The dir has been read!');
     })
 }
 
+//create a directory // { recursive: true } was the key
+//if root directory, then second param is empty string
+const createDir = (path,el = '') => {
+    fs.mkdir(`${path}/${el}`, { recursive: true }, (err) => {
+        if (err) throw err;
+        console.log(`The dir ${path}/${el} has been created!`);
+    })
+}
+
+// create a file
 const createFile = (files) => {
     files.map(file => {
-        
-        fs.writeFile(`${file}`, `//Hello ${file}`, 'utf8', (err) => {
+        //console.log(Object.keys(file)[0]);
+        fs.writeFile(`${Object.keys(file)[0]}`, Object.values(file)[0], 'utf8', (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
         });
     })
 }
 
-rootPath = './app-name'; //dynamic
+//copy file function
+const copyfile = (originPath,destPath,el) => {
+    return fs.copyFile(`${originPath}/${el}`, `${destPath}/${el}`, (err) => {
+        if (err) throw err;
+        console.log(`The file ${el} has been copied to the destination!`);
+    });
+}
 
-//list of directories
-const dirs = [
-    `${rootPath}/dist`,
-    `${rootPath}/src`,
-]
-
-const files = [
-    `${rootPath}/webpack.config.js`,
-    `${rootPath}/package.json`,
-    `${rootPath}/src/index.js`,
-    `${rootPath}/src/index.html`,
-    `${rootPath}/dist/index.html`,
-]
-
-createDir(dirs);
-createFile(files);
+//action
+const copyDir = (originPath, destPath) => {
+    readDir(originPath).forEach(el => {
+        if(el.includes(".")){
+            copyfile(originPath,destPath, el)
+        }else{
+            createDir(destPath, el)
+        }
+    })
+}
 
 
 
+
+// make these dynamic?
+const destPath = './app-name'; //dynamic
+const srcDestPath = `${destPath}/src`;
+const distDestPath = `${destPath}/dist`;
+const originPath = './bin/content'; //not dynamic
+const srcOriginPath = `${originPath}/src`; //get from readDir function it returns an array
+const distOriginPath = `${originPath}/dist`; //get from readDir function it returns an array
+
+
+//list of directories use .push to build this or recursion
+const dirs = []
+const files = []
+    
+
+createDir(destPath);//creates the root directory of the project
+copyDir(originPath, destPath);//creates the children files and directories
+copyDir(srcOriginPath, srcDestPath);//creates the grandchildren files and directories
+copyDir(distPath, distRootPath);//more grandchildren
